efee611292fbc6dde79ab3dcc934361b304024b7
Csaba Palfi <csaba@palfi.me>
2018-07-03 06:02:29 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const util = require('util');
+const {promisify} = require('util');
 const {exec} = require('child_process');
 
 const [,,path] = process.argv;
@@ -15,7 +15,9 @@ function registerMutationObserver() {
             (({addedNodes = []}) => addedNodes.forEach(
                 node => {
                     if (node.id && node.id.startsWith('mall_post_')) {
-                        console.log('permalink/' + node.id.replace(/:.*/, '').replace('mall_post_', ''))
+                        const permalinkId = node.id
+                            .replace(/:.*/, '').replace('mall_post_', '');
+                        console.log(`permalink/${permalinkId}`);
                     }
                 }
             ))
@@ -25,42 +27,46 @@ function registerMutationObserver() {
 }
 
 async function scroll(page, scrollDelay = 1000) {
-  try {
     let previousHeight;
     while (mutationsSinceLastScroll > 0 || initialScrolls > 0) {
-      mutationsSinceLastScroll = 0;
-      initialScrolls--;
-      previousHeight = await page.evaluate('document.body.scrollHeight');
-      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`, {timeout: 0});
-      await page.waitFor(scrollDelay);
+        mutationsSinceLastScroll = 0;
+        initialScrolls--;
+        try {
+            previousHeight = await page.evaluate(
+                'document.body.scrollHeight'
+            );
+            await page.evaluate(
+                'window.scrollTo(0, document.body.scrollHeight)'
+            );
+            await page.waitForFunction(
+                `document.body.scrollHeight > ${previousHeight}`,
+                {timeout: 600000}
+            );
+        } catch(e) {
+            console.log('Scroll failed', e);
+        }
+        await page.waitFor(scrollDelay);
     }
-  } catch(e) {
-      console.log(e);
-  }
 }
 
-
-
 (async () => {
-  const {stdout: browserWSEndpoint} = await util.promisify(exec)('./chrome.sh');
-  const browser =  await puppeteer.connect({browserWSEndpoint});
-  const page = await browser.newPage();
-  page.setViewport({ width: 1280, height: 926 });
-  page.setDefaultNavigationTimeout(0);
+    const {stdout: browserWSEndpoint} = await promisify(exec)('./chrome.sh');
+    const browser = await puppeteer.connect({browserWSEndpoint});
+    const page = await browser.newPage();
+    page.setViewport({ width: 1280, height: 926 });
 
-  await page.goto(`https://www.facebook.com/groups/${path}/`, {timeout: 0});
+    await page.goto(`https://www.facebook.com/groups/${path}/`);
 
-  await page.evaluate(registerMutationObserver);
+    await page.evaluate(registerMutationObserver);
 
-  page.on('console', async (msg) => {
-    if (msg._text && msg._text.startsWith('permalink')) {
-        console.log(msg._text);
-        mutationsSinceLastScroll++;
-    }
-  })
+    page.on('console', async (msg) => {
+        if (msg._text && msg._text.startsWith('permalink')) {
+            console.log(msg._text);
+            mutationsSinceLastScroll++;
+        }
+    });
 
-  await scroll(page);
+    await scroll(page);
 
-  await browser.close();
+    await browser.close();
 })();
--- mo.js
+++
@@ -1,9 +0,0 @@
-const target = document.querySelector('[aria-label="News Feed"]');
-const observer = new MutationObserver(
-    (records) => records.forEach(
-        (({addedNodes}) => addedNodes.forEach(
-            node => console.log('permalink/' + node.id.replace(/:.*/, '').replace('mall_post_', ''))
-        ))
-    )
-);
-observer.observe(target, {childList: true});
